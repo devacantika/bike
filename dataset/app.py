d9ab806a4c110e5247db946e132e6f98f7e13251
devacantika <126942563+devacantika@users.noreply.github.com>
2024-10-08 22:28:44 +0700

    Update app.py

--- dataset/app.py
+++ dataset/app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Sidebar gambar dan judul
-st.sidebar.image("download.png", use_column_width=True)
+st.sidebar.image("dataset/download.png", use_column_width=True)
 st.sidebar.title("Bike Sharing Dashboard")
 st.sidebar.markdown("### Analisis Penggunaan Sepeda")
 
